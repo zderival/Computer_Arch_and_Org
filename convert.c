7a6b3db3cb09d525f1d87b0bb4ff29b1d48e71cd
Zachary Derival <zderival@gmail.com>
2025-09-27 18:09:00 -0400

    Add files via upload

--- convert.c
+++ convert.c
@@ -74,3 +74,116 @@ void print_tables(uint32_t n) {
         bin, oct, masked, hex);
 }
 
+void oct_to_bin(const char *oct, char *out) {
+    int j = 0;
+    for (int i=0; oct[i] != '\0'; i++) {
+        int digit = oct[i] - '0';
+        out[j++] = (digit >> 2) & 1 ? '1':'0';
+        out[j++] = (digit >> 1) & 1 ? '1':'0';
+        out[j++] = (digit >> 0) & 1 ? '1':'0';
+    }
+    out[j] = '\0';
+}
+void oct_to_hex(const char *oct, char *out) {
+    char bin[128];
+    oct_to_bin(oct,bin); // Turning oct's into binary numbers
+
+    int len = strlen(bin); // getting the length of the buffer
+    int padding = (4 - (len % 4)) % 4; // (len % 4) is checking how many bits are left hanging after checking a group of 4 bits.
+    //If there is bits remaining (4 - the remainder) checks how many 0's you need to pad to fill in the hexadecimal
+    // The last % 4 is just checking for security to see if you have it all.
+    // In general, padding is holding the 0's you need to pad your binary number with
+
+    char padded[128]; // padded is holding the padded binary digits that were made with padding.
+    // These will be used to make the hexadecimals
+
+    // This loop is doing the padding:
+    for (int i = 0; i < padding; i++) {
+        padded[i] = '0';
+    }
+    strcpy(padded + padding, bin); // padded is filled with 0's bin (binary num) is getting added into padded
+
+    int j = 0; // Tracker for where you are in out buffer
+    // The loop is taking 4 bits at a time
+    for (int i = 0; i < strlen(padded); i += 4) {
+        // val, is converting the bit to a number, then shifting that number 2^3,2^2,2^1,or 2^0 times
+        // shifting is: x << n == x * 2^n
+        int val = (padded[i] - '0') << 3 |
+                  (padded[i+1] - '0') << 2 |
+                  (padded[i+2] - '0') << 1 |
+                  (padded[i+3] - '0');
+        // converting the val to a hex character
+        // if the value is 0-9 then the hex is 0-9
+        if (val < 10)
+            out[j++] = val + '0';
+        // else if the value is 10 or more: the hex letters are A–F
+        else
+            out[j++] = (val - 10) + 'A';
+    }
+    // closes the buffer after everything is done
+    out[j] = '\0';
+
+}
+void hex_to_bin(const char *hex, char *out) {
+    int j = 0;
+    for (int i = 0; hex[i] != '\0'; i++ ) {
+        int val = 0;
+        if ('0' <= hex[i] && hex[i] <= '9') {
+            val = hex[i] - '0';
+        }else if ('A' <= hex[i] && hex[i] <= 'F') {
+            val = hex[i] - 'A' + 10;
+        }else if ('a' <= hex[i] && hex[i] <= 'f') {
+            val = hex[i] - 'a' + 10;
+        }
+        out[j++] = (val >> 3) & 1 ? '1':'0';
+        out[j++] = (val >> 2) & 1 ? '1':'0';
+        out[j++] = (val >> 1) & 1 ? '1':'0';
+        out[j++] = (val >> 0) & 1 ? '1':'0';
+    }
+    out[j] = '\0';
+}
+
+void to_sign_magnitude(int32_t n, char *out) {
+    uint32_t magnitude;
+
+    if (n >= 0) {
+        magnitude = (int32_t)n;
+        out[0] = '0';
+    }else {
+        magnitude = (int32_t)(-n);
+        out[0] = '1';
+    }
+
+    for (int i = 31; i > 0; i--) {
+        out[i] = (magnitude & 1) ? '1' : '0';
+        magnitude >>= 1;
+    }
+    out[32] = '\0';
+}
+void to_ones_complement(int32_t n, char *out) {
+    if (n >= 0) {
+        out[0] = '0';
+    }else {
+        out[0] = '1';
+        n = -n;
+    }
+    for (int i = 31; i >= 0; i--) {
+        out[i] = (n >> (32-i)) & 1 ? '1' : '0';
+    }
+    out[32] = '\0';
+}
+void to_twos_complement(int32_t n, char *out) {
+    uint32_t val;
+
+    if (n >= 0) {
+        val =(int32_t)n;
+    }else {
+        val = (uint32_t)(-n);
+        val = ~val + 1;
+    }
+
+    for (int i = 31; i >= 0; i--) {
+        out[31 - i] = (val >> i) & 1 ? '1' : '0';
+    }
+    out[32] = '\0';
+}
--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+
 void div_convert(uint32_t n, int base, char *out);
 void sub_convert(uint32_t n, int base, char *out);
 void print_tables(uint32_t n);
 
+void oct_to_bin(const char *oct, char *out);
+void oct_to_hex(const char *oct, char *out);
+void hex_to_bin(const char *oct, char *out);
+
+void to_sign_magnitude(int32_t n, char *out);
+void to_ones_complement(int32_t n, char *out);
+void to_twos_complement(int32_t n, char *out);
+
 int main(void) {
-    char buffer[33];
     char out[65];
-    FILE *file = fopen("a1_test_file.txt", "r");
+    FILE *file = fopen("a2_test.txt", "r");
     if (file == NULL) {
         printf("File could not be open\n");
         return 1;
@@ -36,7 +44,8 @@ int main(void) {
                 printf("Test %d: div_convert(%d, %d) -> Expected: %s, Got: %s [FAIL]\n"
                 ,counter++, num, base,expected,out);}
 
-        }else if (strncmp(line, "sub_convert", 11) == 0) {
+        }
+        else if (strncmp(line, "sub_convert", 11) == 0) {
             int num, base;
             char expected[65];
 
@@ -51,15 +60,97 @@ int main(void) {
                 printf("Test %d: sub_convert(%d, %d) -> Expected: %s, Got: %s [FAIL]\n"
                 ,counter++, num, base,expected,out);
             }
-        }else if ((strncmp(line, "print_tables", 12) == 0)) {
+        }
+        else if ((strncmp(line, "print_tables", 12) == 0)) {
             int num;
             sscanf(line + 13, "%d", &num);
             printf("Test %d: print_tables(%d) -> [FORMATTED_OUTPUT] [PASS]\n", counter++, num);
             pass_counter++;
+        }
+
+        else if (strncmp(line, "oct_to_bin", 10) == 0) {
+            char input[65], expected[65];
+            sscanf(line + 11, "%s %s", input, expected);
+            oct_to_bin(input, out);
+            if (strcmp(out, expected) == 0) {
+                printf("Test %d: oct_to_bin(%s) -> Expected: %s, Got: %s [PASS]\n",
+                    counter++, input, expected, out);
+                pass_counter++;
+            }else {
+                printf("Test %d: oct_to_bin(%s) -> Expected: %s, Got: %s [FAIL]\n",
+                    counter++, input, expected, out);
+            }
+        }
+        else if (strncmp(line, "oct_to_hex", 10) == 0) {
+            char input[65], expected[65];
+            sscanf(line + 11, "%s %s", input, expected);
+            oct_to_hex(input, out);
+            if (strcmp(out, expected) == 0) {
+                printf("Test %d: oct_to_hex(%s) -> Expected: %s, Got: %s [PASS]\n",
+                    counter++, input, expected, out);
+                pass_counter++;
+            } else {
+                printf("Test %d: oct_to_hex(%s) -> Expected: %s, Got: %s [FAIL]\n",
+                    counter++, input, expected, out); }
+        }
+        else if (strncmp(line, "hex_to_bin", 10) == 0) {
+            char input[65], expected[65];
+            sscanf(line + 11, "%s %s", input, expected);
+            hex_to_bin(input, out);
+            if (strcmp(out, expected) == 0) {
+                printf("Test %d: hex_to_bin(%s) -> Expected: %s, Got: %s [PASS]\n",
+                    counter++, input, expected, out);
+                pass_counter++;
+            } else {
+                printf("Test %d: hex_to_bin(%s) -> Expected: %s, Got: %s [FAIL]\n",
+                    counter++, input, expected, out);
+            }
+        }
+
+        else if (strncmp(line, "to_sign_magnitude", 17) == 0) {
+            int32_t num;
+            char expected[65];
+            sscanf(line + 18, "%d %s", &num, expected);
+            to_sign_magnitude(num, out);
+            if (strcmp(out, expected) == 0) {
+                printf("Test %d: to_sign_magnitude(%d) -> Expected: %s,\n Got: %s [PASS]\n",
+                    counter++, num, expected, out);
+                pass_counter++;
+            } else {
+                printf("Test %d: to_sign_magnitude(%d) -> Expected: %s,\n Got: %s [FAIL]\n",
+                    counter++, num, expected, out);
+            }
+        }
+        else if (strncmp(line, "to_ones_complement", 18) == 0) {
+            int32_t num;
+            char expected[65];
+            sscanf(line + 19, "%d %s", &num, expected);
+            to_ones_complement(num, out);
+            if (strcmp(out, expected) == 0) {
+                printf("Test %d: to_ones_complement(%d) -> Expected: %s,\n Got: %s [PASS]\n",
+                    counter++, num, expected, out);
+                pass_counter++;
+            } else {
+                printf("Test %d: to_ones_complement(%d) -> Expected: %s,\n Got: %s [FAIL]\n",
+                    counter++, num, expected, out);
+            }
+        }
+        else if (strncmp(line, "to_twos_complement", 18) == 0) {
+            int32_t num;
+            char expected[65];
+            sscanf(line + 19, "%d %s", &num, expected);
+            to_twos_complement(num, out);
+            if (strcmp(out, expected) == 0) {
+                printf("Test %d: to_twos_complement(%d) -> Expected: %s,\n Got: %s [PASS]\n",
+                    counter++, num, expected, out);
+                pass_counter++;
+            } else {
+                printf("Test %d: to_twos_complement(%d) -> Expected: %s,\n Got: %s [FAIL]\n",
+                    counter++, num, expected, out);
+            }
         }
     }
     fclose(file);
     printf("Summary: %d/%d tests passed", pass_counter, counter);
     return 0;
 }
-
